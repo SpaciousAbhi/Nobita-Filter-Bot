5f34d2ce143a10681acf4e1108243a2cd93f97c6
SpaciousAbhi <74132323+SpaciousAbhi@users.noreply.github.com>
2024-03-17 14:15:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,7 +43,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", True))
 
 # MongoDB information
-DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://madadi9597:1QriSE1XaKW9sBxz@cluster0.dqo97wg.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://tajelo8566:ZWZbqAGHgT0r4jeT@cluster0.xndk1kc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Files')
 
